43cced4918a23095085c6c7975973cc3fa539069
Kristýna Švandová <kristyna.svandova@gmail.com>
2022-05-28 15:24:30 +0200

    Cvičení 15 done

--- src/index.jsx
+++ src/index.jsx
@@ -1,49 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
+import { Joke } from './Joke';
+import { jokes } from './jokes.js';
 
-const App = () => {
-  const [likes, setLikes] = useState(0);
-  const [dislikes, setDislikes] = useState(0);
-  return (
-    <div className="container">
-      <div className="joke">
-        <div className="joke__body">
-          <div className="joke__user">
-            <img
-              className="user-avatar"
-              src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-            />
-            <p className="user-name">Neroxx</p>
-          </div>
-
-          <p className="joke__text">
-            The secret service isn't allowed to yell "Get down!" anymore when
-            the president is about to be attacked. Now they have to yell
-            "Donald, duck!"
-          </p>
-        </div>
-        <div className="joke__likes">
-          <button
-            id="btn-up"
-            className="btn-like btn-like--up"
-            onClick={() => setLikes(likes + 1)}
-          ></button>
-          <span id="likes-up" className="likes-count likes-count--up">
-            {likes}
-          </span>
-          <button
-            id="btn-down"
-            className="btn-like btn-like--down"
-            onClick={() => setDislikes(dislikes + 1)}
-          ></button>
-          <span id="likes-down" className="likes-count likes-count--down">
-            {dislikes}
-          </span>
-        </div>
-      </div>
-    </div>
-  );
-};
-
+const App = () => (
+  <div className="container">
+    {jokes.map((joke) => (
+      <Joke
+        key={joke.id}
+        userAvatar={joke.avatar}
+        userName={joke.name}
+        text={joke.text}
+        likes={joke.likes}
+        dislikes={joke.dislikes}
+      />
+    ))}
+  </div>
+);
 createRoot(document.querySelector('#app')).render(<App />);
